8a294bd462167b634d1d7a8daedaca04b997d47a
Roland-Sankara <roland1sankara@gmail.com>
2024-04-23 21:27:35 +0300

    add integration tests

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,6 +1,9 @@
 import sum from '../helper-functions/user.js';
 import app from "../server.js";
 import request from "supertest";
+import "dotenv/config";
+
+let token = process.env.JWT_TOKEN
 
 test('two plus two is four', () => {
     expect(sum(2,2)).toBe(4);
@@ -13,11 +16,11 @@ test('auth header is missing', async ()=>{
 })
 
 test('get all users with 200 status code', async ()=>{
-    let response = await request(app).get('/users').set('authorization', "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTmV3IENlZHJpYyIsInVzZXJJZCI6NCwidXNlclJvbGUiOiJBRE1JTiIsImlhdCI6MTcxMzU0NzQwOCwiZXhwIjoxNzEzNjMzODA4fQ.TT0Se6iJIZJdwMhVFGgBvsO2EEOvrH7xmGTqe8ZT96g")
+    let response = await request(app).get('/users').set('authorization', `Bearer ${token}`)
     expect(response.status).toEqual(200)
 } )
 
 test('get all users with response object having the success prop being true', async ()=>{
-    let response = await request(app).get('/users').set('authorization', "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTmV3IENlZHJpYyIsInVzZXJJZCI6NCwidXNlclJvbGUiOiJBRE1JTiIsImlhdCI6MTcxMzU0NzQwOCwiZXhwIjoxNzEzNjMzODA4fQ.TT0Se6iJIZJdwMhVFGgBvsO2EEOvrH7xmGTqe8ZT96g")
+    let response = await request(app).get('/users').set('authorization', `Bearer ${token}`)
     expect(response.body.success).toEqual(true)
 } )
